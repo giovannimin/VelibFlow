eef5320b9d277638393e60ce4c25dbb60ecf9039
Giovanni Minghelli <105241670+giovannimin@users.noreply.github.com>
2023-06-27 20:51:03 +0200

    update workflow

--- shell_script/download_history.sh
+++ shell_script/download_history.sh
@@ -4,16 +4,23 @@ set -e
 
 base="https://api.github.com/repos/giovannimin/VelibFlow/releases/latest"
 
+# Récupère les URL de téléchargement des fichiers depuis l'API GitHub
 urls=$(
     curl --fail --retry 8 --retry-delay 0 "$base" |
     jq -r '.assets|sort_by(.updated_at)|reverse[]|.browser_download_url'
 )
 
 outfile=velib_data.zip
+
+# Parcourt les URLs de téléchargement jusqu'à trouver un fichier valide
 for url in $urls; do
+    # Télécharge le fichier ZIP depuis l'URL
     curl --location --fail --retry 8 --retry-delay 0 --output "$outfile" "$url" &&
+    # Décompresse le fichier ZIP
     unzip -o $outfile &&
+    # Sort de la boucle si le téléchargement et la décompression ont réussi
     break || continue
 done
 
+# Supprime le fichier ZIP
 rm $outfile
--- shell_script/update_data.sh
+++ shell_script/update_data.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+# Télécharge l'historique des données
+bash -x ./download_history.sh
+
+# Effectue la récupération des nouvelles données
 python3 ./sources/fetch_data.py
+
+# Met à jour la release sur GitHub
+bash -x ./update_release.sh
